664a66fd8d6536666d8c433f1159445736baa02c
Julian Arango <takzuo@gmail.com>
2022-02-08 15:19:48 -0500

    firebase firestore ract and .env

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+import { collection, getDocs } from "firebase/firestore";
+import db from "./firebase/firebaseConfig";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+     useEffect(() => {
+    
+    const obtenerdatos = async () => {
+       const datos = await getDocs(collection(db, 'posts')); // la coleccion posts
+       // console.log(datos.docs[0].data());
+
+       datos.forEach((documento) => {
+          console.log(documento.data());
+       })
+    }
+
+    obtenerdatos();
+ 
+  }, []);
+
+  return( 
+    <h1>Hello World</h1>
   );
+
 }
 
 export default App;
